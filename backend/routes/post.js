440e73bf1497a789ca5cb1779a77943a10436939
crist51 <crist.degorre@gmail.com>
2022-07-05 02:57:25 +0200

    1.01

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const postCtrl = require('../controllers/post');
-const commentCtrl = require('../controllers/comment')
 const auth = require('../middleware/auth')
 
 router.post('/', auth, postCtrl.createPost);
--- frontend/src/components/Gallery/ReadGallery.jsx
+++ frontend/src/components/Gallery/ReadGallery.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import logo from '../../assets/logo/icon.png';
 
 // //---------------------------------------------------
 
@@ -19,7 +20,8 @@ function ReadGallery() {
 
     const fetchData = async () => {
       const result = await axios("http://localhost:3000/api/gallery", config)
-      setData(result.data.results);
+      setData(result.data.results)
+      console.log(result.data.results);
     };
 
     fetchData();
@@ -32,7 +34,7 @@ function ReadGallery() {
           <Link to={"oneGallery/?id=" + item.gallery_id} title={"lien vers post multimedia : "+ item.gallery_titre}>
             <article className="article_img">
               <h2>{item.gallery_titre}</h2>
-              <img alt="post multimedia" src={item.gallery_media ||"http://localhost:3000/images/icon.png1655753820253.png"} />
+              <img alt="post multimedia" src={item.gallery_media} />
             </article>
           </Link>
         ))}
